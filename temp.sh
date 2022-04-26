4d575b6bb600d968acc86f466b245fb6b3fb4f56
Mywifi <vincent-ye@live.cn>
2022-04-26 22:07:45 +0800

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -3,12 +3,12 @@
 key=`cat ./key`
 machine=`hostname`
 
-while sleep 5
+while sleep 60
 do
   date
   t=`sensors |grep Package|awk '{print $4}'| cut -b 2,3`
   echo 'current temperature is: '$t'°C'
-  if [ "$t" -gt "50" ];then
+  if [ "$t" -gt "90" ];then
     echo 'temperature warning...'
     txt="$machine temperature too high $t"
     curl "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$key" \
