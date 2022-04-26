879146bb98d735b4ab852ebe688c16da0dc85fd0
Mywifi <vincent-ye@live.cn>
2022-04-26 18:35:12 +0800

    Update msg.sh

--- msg.sh
+++ msg.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-$key=`cat ./key`
-$txt=`cat ./msg.txt`
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$key' \
-   -H 'Content-Type: application/json' \
-   -d '
-   {
-     "msgtype": "text",
-     "text": {
-         "content": "$txt"
-     }
-   }'
+
